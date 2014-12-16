a0c68d9b1c9ea5371cc3c427dc5e2e702248c424
Manolis Papidakis <papidakis@gmail.com>
2014-12-16 13:37:16 +0200

    user mvc

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   validates :name,presence:true
   validates :email,presence:true ,format:/\A\S+@\S+\z/,
              uniqueness: {case_sensitive:false}
-  validates :pasword ,length:{minimum:3,allow_blank:true}
+  validates :password ,length:{minimum:3,allow_blank:true}
+  validates :username ,presence:true,format:/\A[A-Z0-9]+\z/i,
+             uniqueness:{case_insensitive:true}
 end
---
+++ db/migrate/20141216112510_add_username_field_to_users.rb
@@ -0,0 +1,6 @@
+class AddUsernameFieldToUsers < ActiveRecord::Migration
+  def change
+    add_column :users ,:username,:string
+
+  end
+end
