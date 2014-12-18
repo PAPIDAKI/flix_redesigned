94edd6444ebd74bd0788077d81ec4cb3e3525f5a
Manolis Papidakis <papidakis@gmail.com>
2014-12-18 08:08:29 +0200

    password authentication for sign in create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,16 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		params[:emal]
-		params[:password]
+		if user=User.authenticate(params[:email],params[:password])
+
+			session[:user_id]=user.id
+			flash[:notice]="Welcome back ,#{user.name}"
+			redirect_to user
+		else
+			flash.now[:alert]="Invalid email or password"
+			render :new
+		#redirect to user profile page with cookie
+		#params[:password]
+		end
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,17 @@ class User < ActiveRecord::Base
              uniqueness:{case_insensitive:true}
 
 
+
+  def self.authenticate(email,password)
+  user=User.find_by(email:email )
+  user && user.authenticate(password)
+
+  end
+
+
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase)
   end
+
+
 end
