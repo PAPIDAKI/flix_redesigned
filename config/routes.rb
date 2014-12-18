073c9b884693b028e74d2b44c767e82361c45917
Manolis Papidakis <papidakis@gmail.com>
2014-12-18 07:15:02 +0200

    add session signin path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Flix::Application.routes.draw do
+  get 'signin'=>"sessions#new"
   get 'signup'=>"users#new"
   resources :users
+  resource :session
 
   root "movies#index"
   resources :movies do
